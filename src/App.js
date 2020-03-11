56f58b74b5f7ee1f8e53a1c5490add231b0e28d5
millord <juancarlosmillord@gmail.com>
2020-03-10 23:28:31 -0400

    fishing the app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import MovieList from "./MovieList";
 import Nav from "./Nav";
 import { MovieProvider } from "./MovieContext";
+import AddMovie from "./AddMovie";
 
 const App = () => {
   return (
     <MovieProvider>
       <div>
         <Nav />
+        <AddMovie />
         <MovieList />
       </div>
     </MovieProvider>
